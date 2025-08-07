56c5004c4387066806c8bbc4be8202affebbb82c
SirGii1 <sageeradamu54321@gmail.com>
2025-08-07 06:56:48 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,26 @@
+import { AppProps } from 'next/app';
+import { FC } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
 
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const wallets = [new PhantomWalletAdapter()];
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const endpoint = process.env.NEXT_PUBLIC_QUICKNODE_ENDPOINT || clusterApiUrl('devnet');
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default App;
